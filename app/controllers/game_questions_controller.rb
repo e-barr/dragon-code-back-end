11d9ee3e7684e6fb43bbc4151982cd64a528c274
farverio <github email address>
2018-09-11 16:53:16 -0500

    edited game_question_controller

--- app/controllers/game_questions_controller.rb
+++ app/controllers/game_questions_controller.rb
@@ -5,7 +5,7 @@ class GameQuestionsController < ApplicationController
   end
   
   def create
-    game_id = params(:game_id)
+    game_id = game_question_params(params[:game_id])
     
     new_question = GameQuestion.new(game: game_id)
     new_question.get_new_question(game_id)
@@ -13,12 +13,19 @@ class GameQuestionsController < ApplicationController
     
     formatted_json = new_question.format_new_question_hash
     render json: formatted_json
+  end
+  
+  def update
+    updated_data = game_question_params(params)
+    
+    game = GameQuestion.find_by(id: updated_data[:id])
+    game.update(user_answer: updated_data[:user_answer])
     
   end
   
   private
   
   def game_question_params
-    params.require(:game_question).permit(:game_id)
+    params.require(:game_question).permit(:id, :game_id, :question_id, :user_answer)
   end
 end
