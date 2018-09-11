fca16f12c1ca296725250517d6f9d5cc249ac2c4
e-barr <github email address>
2018-09-11 16:57:06 -0500

    Update model methods

--- app/models/game_question.rb
+++ app/models/game_question.rb
@@ -5,4 +5,20 @@ class GameQuestion < ApplicationRecord
   def update_score(user_response)
     question.correct_answer?(user_response) ? score += question.difficulty_level : score
   end
+
+  def get_question
+    asked_questions = GameQuestion.all.select { |gq| gq.game_id == self.game_id }.map{ |gq2| gq2.id }
+
+    new_question = false
+
+    until new_question
+      question = Question.all.sample
+
+      unless asked_questions.include?(question.id)
+        new_question = true
+        self.question_id = question.id
+        return question
+      end
+    end
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,22 @@
 class Question < ApplicationRecord
   has_many :game_questions
-
-  def options
-    [self.option1, self.option2, self.option3]
-  end
+  all_options_hash = {}
 
   def correct_answer?(user_answer)
     user_response == self.answer
   end
+
+  def format_new_question_hash
+    {
+      q_id: self.id,
+      question_text: self.content,
+      answer: self.answer,
+      options: {
+        "1": self.option1,
+        "2": self.option2,
+        "3": self.option3
+      }
+    }
+  end
+
 end
