e9c803c44ae01578f993d89fab07a3c0ba4c2a0c
e-barr <github email address>
2018-09-11 20:52:09 -0500

    Update models

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,10 +2,32 @@ class Game < ApplicationRecord
   validates :username, presence: true
   belongs_to :user
   has_many :game_questions
+  after_initialize :set_default_score, :create_and_or_associate_new_user
 
-  def initialize(username)
-    @username = username
-    @score = 0
+  @@high_score = 0
+
+  def create_and_or_associate_new_user
+    self.user ||= self.create_new_user
+  end
+
+  def create_new_user
+    User.create(username: self.username)
+  end
+
+  def set_default_score
+    self.score = 0
+  end
+
+  def self.high_score
+    @@high_score
+  end
+
+  def self.set_high_score(new_score)
+    new_score > self.high_score ? @@high_score = new_score : self.high_score
+  end
+
+  def update_score(total_points)
+    self.score += total_points
   end
 
 end
--- app/models/game_question.rb
+++ app/models/game_question.rb
@@ -2,10 +2,6 @@ class GameQuestion < ApplicationRecord
   belongs_to :game
   belongs_to :question
 
-  def update_score(user_response)
-    question.correct_answer?(user_response) ? score += question.difficulty_level : score
-  end
-
   def get_question
     asked_questions = GameQuestion.all.select { |gq| gq.game_id == self.game_id }.map{ |gq2| gq2.id }
 
